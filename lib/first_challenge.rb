b03de4a45450a10defbf9e5248980a1892a8832f
Jeong Park <wjdtn1220@gmail.com>
2019-08-18 22:52:29 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,10 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = {
+      :montague => {
+       :patriarch => {
+         :name => "Lord Monatague",
+         :age => "53"
+       }
+      }
+   }
 end
